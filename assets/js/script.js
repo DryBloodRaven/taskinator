79a7aa13499e44e12ecaaa9572d2b30e58f28022
DryBloodRaven <amproctor@gmail.com>
2020-10-16 00:06:45 -0700

    defined drop zones

--- assets/js/script.js
+++ assets/js/script.js
@@ -111,7 +111,6 @@ var createTaskActions = function(taskId) {
     return actionContainerEl;
 };
 
-formEl.addEventListener("submit", taskFormHandler);
 
 var taskButtonHandler = function(event) {
     // get target element from even
@@ -172,7 +171,7 @@ var taskStatusChangeHandler = function (event) {
     // find the parent task item element based on the id
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
 
-    if (statusValue === "to do") {
+    if (statusValue === "to-do") {
         tasksToDoEl.appendChild(taskSelected);
     } else if (statusValue === "in progress") {
         tasksInProgressEl.appendChild(taskSelected);
@@ -181,5 +180,23 @@ var taskStatusChangeHandler = function (event) {
     }
 };
 
+var dragTaskHandler = function(event) {
+    var taskId = event.target.getAttribute("data-task-id");
+    event.dataTransfer.setData("text/plain", taskId);
+
+    var getId = event.dataTransfer.getData("text/plain");
+    console.log("getId:", getId, typeof getId);
+};
+
+var dropZoneDragHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        event.preventDefault();
+    }
+};
+
+formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+pageContentEl.addEventListener("dragstart", dragTaskHandler);
+pageContentEl.addEventListener("dragover", dropZoneDragHandler);
